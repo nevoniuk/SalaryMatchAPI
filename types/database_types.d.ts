1e47455dff39a3a89b365c42c511adeb42f4a410
Ethan Falck <ethan.falck@gmail.com>
2023-02-07 21:56:33 -0500

    Add get city demographics endpoint

---
+++ types/database_types.d.ts
@@ -0,0 +1,22 @@
+export interface City {
+    id: string,
+    name: string,
+    monthly_internet_price: number,
+    total_population: number,
+    caucasian_population: number,
+    black_population: number,
+    native_american_population: number,
+    asian_population: number,
+    pacific_islander_population: number,
+    hispanic_population: number,
+    population_under_25: number,
+    population_25_to_29: number,
+    population_30_to_34: number,
+    population_35_to_44: number,
+    population_45_to_54: number,
+    population_55_to_64: number,
+    population_65_to_74: number,
+    population_75_and_older: number,
+    male_population: number,
+    female_population: number
+}
