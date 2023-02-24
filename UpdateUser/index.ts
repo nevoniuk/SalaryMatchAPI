4dcf2c3509bb78df8d40103476e90e467c8bd10c
Herman Saini <herman.saini@gmail.com>
2023-02-24 14:33:39 -0500

    Added Authentication to Funcitons
    Added authentication via token for personalized functions for users

--- UpdateUser/index.ts
+++ UpdateUser/index.ts
@@ -1,13 +1,31 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-
-
+import { validateToken
+ } from "../utility/validateToken";
 const updateUser: AzureFunction = async (context: Context, req: HttpRequest, outputDocument: any): Promise<void> => {
+    const user_id = await validateToken(req.headers);
+
+    if (user_id == "No Token Found") {
+        context.res = {
+            status: 401,
+            body: "Token not found."
+        }
+        return;
+    }
+
+    if (user_id == "Invalid Session") {
+        context.res = {
+            status: 401,
+            body: "Token invalid."
+        }
+        return;
+    }
+
     if (context.req && context.req.body && context.req.body.id && context.req.body.password && context.req.body.degree
         && context.req.body.gender && context.req.body.race && context.req.body.temperature_preference 
         && context.req.body.humidity_preference && context.req.body.sunlight_preference && context.req.body.demographic_preference
         && context.req.body.salary_preference && context.req.body.pto_preference && context.req.body.spender_type) {
             context.bindings.outputDocument = JSON.stringify({
-                id: context.req.body.id,
+                id: user_id,
                 password: context.req.body.password,
                 degree: context.req.body.degree,
                 gender: context.req.body.gender,
