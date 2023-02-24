2e15ccaf74a6b129818ed0f9fb5c3b81f97da721
Ethan Falck <ethan.falck@gmail.com>
2023-02-24 17:23:13 -0500

    removeRemove some user creation options

--- CreateUser/index.ts
+++ CreateUser/index.ts
@@ -2,10 +2,9 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import { usersContainer } from "../cosmosClient";
 
 const createUser: AzureFunction = async (context: Context, req: HttpRequest, outputDocument: any): Promise<void> => {
-    if (context.req && context.req.body && context.req.body.id && context.req.body.password && context.req.body.degree
-        && context.req.body.gender && context.req.body.race && context.req.body.temperature_preference 
+    if (context.req && context.req.body && context.req.body.id && context.req.body.password && context.req.body.temperature_preference 
         && context.req.body.humidity_preference && context.req.body.sunlight_preference && context.req.body.demographic_preference
-        && context.req.body.salary_preference && context.req.body.pto_preference && context.req.body.spender_type) {
+        && context.req.body.salary_preference && context.req.body.pto_preference) {
             
             var accountExists = false;
             const item  = await usersContainer.item(context.req.body.id, context.req.body.id).read();
@@ -18,16 +17,15 @@ const createUser: AzureFunction = async (context: Context, req: HttpRequest, out
                 context.bindings.outputDocument = JSON.stringify({
                     id: context.req.body.id,
                     password: context.req.body.password,
-                    degree: context.req.body.degree,
-                    gender: context.req.body.gender,
-                    race: context.req.body.race,
+                    degree: context.req.body.degree ? context.req.body.degree : "None",
+                    gender: context.req.body.gender ? context.req.body.gender : "Unknown",
+                    race: context.req.body.race ? context.req.body.race : "Unknown",
                     temperature_preference: context.req.body.temperature_preference,
                     humidity_preference: context.req.body.humidity_preference,
                     sunlight_preference: context.req.body.sunlight_preference,
                     demographic_preference: context.req.body.demographic_preference,
                     salary_preference: context.req.body.salary_preference,
                     pto_preference: context.req.body.pto_preference,
-                    spender_type: context.req.body.spender_type
                 });
                 context.res = {
                     body: "User added to Cosmos DB"
