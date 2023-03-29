4b7b15b8c84c54b0a5ef846cafbdfef91be6717a
William Yang <s-yangqih@bsd405.org>
2023-03-28 23:02:59 -0400

    getCompanyDetails returns undefined/empty body

---
+++ GetCompanyDetails/tests/index.test.ts
@@ -0,0 +1,51 @@
+import { Context } from '@azure/functions'
+import {
+    stubContextFromBindingDefinitions,
+    createHttpTrigger,
+} from 'stub-azure-function-context'
+import getCompanyDetails from '..'
+import { FakeCompanyGoogle } from '../../mock_data/MockCompanies'
+import { Company } from '../../types/database_types'
+import { companyToBasicCompanyInfo, companyToCompanyDetails} from '../../utility/type_mappings'
+
+describe('GetCompanies tests', () => {
+    // it('Gives 404 on no cities found', async () => {
+    //     const res = await mockedRequestFactory(null)
+
+    //     expect(res.statusCode).toEqual(404)
+
+    //     expect(res.body).toEqual(JSON.stringify('No companies found'))
+    // }),
+
+    // it('Gives returns cities when found in DB', async () => {
+    //     const res = await mockedRequestFactory([FakeCompanyGoogle])
+
+    //     expect(res.body).toEqual(JSON.stringify([companyToBasicCompanyInfo(FakeCompanyGoogle)]));
+    // })
+
+    it('returns company demographcis', async () => {
+        const res = await mockedRequestFactory([FakeCompanyGoogle])
+        console.log(res.body)
+        console.log("HMMM", JSON.stringify([companyToCompanyDetails(FakeCompanyGoogle)]))
+        expect(res.body).toEqual(JSON.stringify([companyToCompanyDetails(FakeCompanyGoogle)]));
+    })
+})
+
+const mockedRequestFactory = async (companies: Company[]) => {
+    const context: Context = stubContextFromBindingDefinitions([
+        {
+            type: 'httpTrigger',
+            name: 'req',
+            direction: 'in',
+            data: createHttpTrigger(
+                'GET',
+                'http://example.com/companies/{test_id}'
+            ),
+        },
+        { type: 'http', name: '$return', direction: 'out' }
+    ])
+
+    await getCompanyDetails(context, context.req, companies);
+    console.log(context.res)
+    return context.res;
+}
--- mock_data/MockCompanies.ts
+++ mock_data/MockCompanies.ts
@@ -2,5 +2,11 @@ import { Company } from "../types/database_types";
 
 export const FakeCompanyGoogle: Company = {
     id: "123",
-    name: "Google"
+    name: "Google",
+    percent_male: 31,
+    percent_female: 69,
+    percent_caucasian: 53,
+    percent_asian: 36,
+    percent_latino: 4,
+    percent_black: 3,
 }
