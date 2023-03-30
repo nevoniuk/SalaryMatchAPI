dfe96f0d19fc562e6b2393b6799dc735a3bb86f2
Herman Saini <herman.saini@gmail.com>
2023-03-30 15:01:22 -0400

    Fixed Comments
    deleted a comment that was not applicable in the getcity file

--- GetCityReviews/index.ts
+++ GetCityReviews/index.ts
@@ -4,10 +4,7 @@ import { cityReviewsContainer } from "../cosmosClient";
 import { CityReview } from "../types/database_types";
 import { responseFactory } from "../utility/response_factory";
 import { cityReviewToBasicCityReviewInfo } from "../utility/type_mappings";
-/*
-    Allows users to search for states and get basic info about them. Matches any states with names that
-    contain the entered search string.
-*/
+
 const getCityReviews: AzureFunction = async (context: Context, req: HttpRequest): Promise<void> => {
     const cityReviewQuery: SqlQuerySpec = {
         query: "SELECT * FROM c WHERE c.city_id = @city_id",
