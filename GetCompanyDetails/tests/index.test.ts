8bdefeba66181d64adcdfd9cfc66acfd7c387877
William Yang <s-yangqih@bsd405.org>
2023-03-30 21:15:43 -0400

    finished getCompanyDetails test

--- GetCompanyDetails/tests/index.test.ts
+++ GetCompanyDetails/tests/index.test.ts
@@ -9,19 +9,14 @@ import { Company } from '../../types/database_types'
 import { companyToBasicCompanyInfo, companyToCompanyDetails} from '../../utility/type_mappings'
 
 describe('GetCompanies tests', () => {
-    // it('Gives 404 on no cities found', async () => {
-    //     const res = await mockedRequestFactory(null)
+    it('Gives 404 on no companies found', async () => {
+        const res = await mockedRequestFactory(null)
 
-    //     expect(res.statusCode).toEqual(404)
+        expect(res.statusCode).toEqual(404)
 
-    //     expect(res.body).toEqual(JSON.stringify('No companies found'))
-    // }),
-
-    // it('Gives returns cities when found in DB', async () => {
-    //     const res = await mockedRequestFactory([FakeCompanyGoogle])
-
-    //     expect(res.body).toEqual(JSON.stringify([companyToBasicCompanyInfo(FakeCompanyGoogle)]));
-    // })
+        expect(res.body).toEqual(JSON.stringify('Company not found'))
+        console.log("YEE", res.body)
+    })
 
     it('returns company demographcis', async () => {
         const res = await mockedRequestFactory(FakeCompanyGoogle)
